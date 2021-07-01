1328df795e5c28b272ecb4c1c270ba29be41e336
denimu <42787839+kurodenimu@users.noreply.github.com>
2021-07-02 02:36:31 +0900

    外部ファイル読込のブックマークレット関連部分を削除。

--- minify.bat
+++ minify.bat
@@ -1,12 +1,8 @@
 @echo off
 setlocal
 
-rem 利用環境に応じてカスタム====================================================
-set BASE_URI=
-rem ============================================================================
-
 set MYPATH=%~dp0
-set HTML=doc\index.html
+set HTML=docs\index.html
 type Template.html > %HTML%
 
 for /f %%a in ('dir /b /s %MYPATH% ^| findstr /r ".js$" ^| findstr /r /v ".min.js$"') do (
@@ -26,7 +22,7 @@ set WKPATH=%~dp1
 
 call :GETRELPATH "%WKPATH:~0,-1%"
 
-set OUTPATH=%MYPATH%doc\%RELPATH%
+set OUTPATH=%MYPATH%docs\%RELPATH%
 if not exist %OUTPATH% mkdir %OUTPATH%
 
 call uglifyjs --compress --mangle -- %1 > %OUTPATH%%MINFILE%
@@ -39,13 +35,6 @@ echo |set /p="<a href='javascript: (function() {">> %HTML%
 type %OUTPATH%%MINFILE% >> %HTML%
 echo |set /p="})()'>%~n1</a>">> %HTML%
 echo.>> %HTML%
-rem echo |set /p="<a href='javascript: (function() {">> %HTML%
-rem echo |set /p="const s=document.createElement("script");s.type="text/javascript";">> %HTML%
-rem echo |set /p="s.src=%BASE_URI% + "/%RELPATH:\=/%%MINFILE%";">> %HTML%
-rem echo |set /p="const es=document.getElementsByTagName("script")[0];es.parentNode.insertBefore(s,es);">> %HTML%
-rem echo |set /p="})()'>%~n1</a>">> %HTML%
-rem echo |set /p="</div>">> %HTML%
-rem echo.>> %HTML%
 
 exit /b
 
